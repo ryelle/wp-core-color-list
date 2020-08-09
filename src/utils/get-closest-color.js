565c783a2316b93e9b9493b9ea58cba7017ddf04
Kelly Dwan <ryelle@users.noreply.github.com>
2020-08-09 12:12:36 -0400

    Add `color-convert` to convert rgb-lab without external library

--- src/utils/get-closest-color.js
+++ src/utils/get-closest-color.js
@@ -1,28 +1,30 @@
+/**
+ * External dependencies
+ */
+import convert from 'color-convert';
 import tinycolor from 'tinycolor2';
-import colors from './colors.json';
 
-export default function getClosestColor( hex, colorList = colors ) {
-	var color = tinycolor( hex );
+export default function getClosestColor( hex, colorList = {} ) {
+	if ( ! Object.values( colorList ).length ) {
+		return false;
+	}
+	const color = tinycolor( hex );
 	if ( ! color || ! color.isValid() ) {
 		return false;
 	}
 	const { r, g, b } = color.toRgb(); // @todo handle transparent colors?
-	const lab = XYZ_to_Lab(
-		D65_to_D50( lin_sRGB_to_XYZ( lin_sRGB( [ r, g, b ] ) ) )
-	);
+	const lab = convert.rgb.lab( [ r, g, b ] );
 	let lowestDiff = 9999999;
 	let matchedColor = '';
 	Object.values( colorList ).forEach( ( value ) => {
 		const valueColor = tinycolor( value );
 		const { r: r2, g: g2, b: b2 } = valueColor.toRgb();
-		const lab2 = XYZ_to_Lab(
-			D65_to_D50( lin_sRGB_to_XYZ( lin_sRGB( [ r2, g2, b2 ] ) ) )
-		);
+		const lab2 = convert.rgb.lab( [ r2, g2, b2 ] );
 		// See https://en.wikipedia.org/wiki/Color_difference
 		const diff = Math.sqrt(
-			Math.pow( lab2[0] - lab[0], 2 ) +
-				Math.pow( lab2[1] - lab[1], 2 ) +
-				Math.pow( lab2[2] - lab[2], 2 )
+			Math.pow( lab2[ 0 ] - lab[ 0 ], 2 ) +
+				Math.pow( lab2[ 1 ] - lab[ 1 ], 2 ) +
+				Math.pow( lab2[ 2 ] - lab[ 2 ], 2 )
 		);
 
 		if ( diff < lowestDiff ) {
--- src/utils/test/get-closest-color.js
+++ src/utils/test/get-closest-color.js
@@ -1,14 +1,19 @@
 /**
  * Internal dependencies
  */
-import getClosestColor from '../get-closest-color';
+import { getClosestColor } from '../';
+import { brandColors } from '../../colors';
 
 test( 'Invalid values should return false.', () => {
-	expect( getClosestColor( '' ) ).toBe( false );
-	expect( getClosestColor( false ) ).toBe( false );
-	expect( getClosestColor( '#' ) ).toBe( false );
-	expect( getClosestColor( 'xyz' ) ).toBe( false );
-	expect( getClosestColor( 32.3 ) ).toBe( false );
+	expect( getClosestColor( '', { red: '#f00' } ) ).toBe( false );
+	expect( getClosestColor( false, { red: '#f00' } ) ).toBe( false );
+	expect( getClosestColor( '#', { red: '#f00' } ) ).toBe( false );
+	expect( getClosestColor( 'xyz', { red: '#f00' } ) ).toBe( false );
+	expect( getClosestColor( 32.3, { red: '#f00' } ) ).toBe( false );
+} );
+
+test( 'Empty color list should return false.', () => {
+	expect( getClosestColor( '#f00', {} ) ).toBe( false );
 } );
 
 test( 'Custom color lists work.', () => {
@@ -19,14 +24,7 @@ test( 'Custom color lists work.', () => {
 	};
 	expect( getClosestColor( '#e00', colors ) ).toBe( '#f00' );
 	expect( getClosestColor( '#a10', colors ) ).toBe( '#f00' );
-	expect( getClosestColor( '#089', colors ) ).toBe( '#00f' );
-} );
-
-test( 'Matching values should return the exact color.', () => {
-	expect( getClosestColor( '#e8eaeb' ) ).toBe( '#e8eaeb' );
-	expect( getClosestColor( '#b02828' ) ).toBe( '#b02828' );
-	expect( getClosestColor( '#826eb4' ) ).toBe( '#826eb4' );
-	expect( getClosestColor( '#008ec2' ) ).toBe( '#008ec2' );
+	expect( getClosestColor( '#089', colors ) ).toBe( '#0f0' );
 } );
 
 test( 'Red correctly returns a red color.', () => {
@@ -37,24 +35,39 @@ test( 'Red correctly returns a red color.', () => {
 	expect( getClosestColor( 'rgb(230, 1, 34)', colors ) ).toBe( '#dc3232' );
 } );
 
+test( 'Matching values should return the exact color.', () => {
+	expect( getClosestColor( '#e8eaeb', brandColors ) ).toBe( '#e8eaeb' );
+	expect( getClosestColor( '#b02828', brandColors ) ).toBe( '#b02828' );
+	expect( getClosestColor( '#826eb4', brandColors ) ).toBe( '#826eb4' );
+	expect( getClosestColor( '#008ec2', brandColors ) ).toBe( '#008ec2' );
+} );
+
 test( 'Hex values match with or without the #.', () => {
-	expect( getClosestColor( '#e8eaeb' ) ).toBe( '#e8eaeb' );
-	expect( getClosestColor( 'e8eaeb' ) ).toBe( '#e8eaeb' );
+	expect( getClosestColor( '#e8eaeb', brandColors ) ).toBe( '#e8eaeb' );
+	expect( getClosestColor( 'e8eaeb', brandColors ) ).toBe( '#e8eaeb' );
 } );
 
 test( 'Nearby values should return the correct color.', () => {
-	expect( getClosestColor( '#103369' ) ).toBe( '#32373c' );
-	expect( getClosestColor( '#97d936' ) ).toBe( '#6bc373' );
-	expect( getClosestColor( '#9995cb' ) ).toBe( '#9b8bc3' );
-	expect( getClosestColor( '#511a29' ) ).toBe( '#32373c' );
+	expect( getClosestColor( '#103369', brandColors ) ).toBe( '#4e426c' );
+	expect( getClosestColor( '#97d936', brandColors ) ).toBe( '#46b450' );
+	expect( getClosestColor( '#9995cb', brandColors ) ).toBe( '#9b8bc3' );
+	expect( getClosestColor( '#511a29', brandColors ) ).toBe( '#23282d' );
 } );
 
 test( 'RGB values work.', () => {
-	expect( getClosestColor( 'rgb(218, 242, 252)' ) ).toBe( '#e6f6fb' );
-	expect( getClosestColor( 'rgb(54, 240, 251)' ) ).toBe( '#66c6e4' );
+	expect( getClosestColor( 'rgb(218, 242, 252)', brandColors ) ).toBe(
+		'#e6f6fb'
+	);
+	expect( getClosestColor( 'rgb(54, 240, 251)', brandColors ) ).toBe(
+		'#66c6e4'
+	);
 } );
 
 test( 'RGBA values work.', () => {
-	expect( getClosestColor( 'rgba( 219, 86, 121, 1 )' ) ).toBe( '#e35b5b' );
-	expect( getClosestColor( 'rgba( 21, 21, 11, 0.6 )' ) ).toBe( '#191e23' );
+	expect( getClosestColor( 'rgba( 219, 86, 121, 1 )', brandColors ) ).toBe(
+		'#e35b5b'
+	);
+	expect( getClosestColor( 'rgba( 21, 21, 11, 0.6 )', brandColors ) ).toBe(
+		'#191e23'
+	);
 } );
