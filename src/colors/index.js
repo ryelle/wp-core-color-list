b7b00b1cd9677cd09b8f2ad08d89f0ffc406dabb
Kelly Dwan <ryelle@users.noreply.github.com>
2020-08-09 12:11:57 -0400

    Move color files to single folder

---
+++ src/colors/index.js
@@ -0,0 +1,3 @@
+export { default as brandColors } from './brand-colors.json';
+export { default as coreColors } from './core-colors.json';
+export { default as newBrandColors } from './new-brand-colors.json';
