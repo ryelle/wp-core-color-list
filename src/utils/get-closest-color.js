763f4d88bee9acb02eb26ff6ac33dc1d3ebc0ecf
Kelly Dwan <ryelle@users.noreply.github.com>
2020-08-09 13:00:24 -0400

    Add option to return diff value

--- src/utils/get-closest-color.js
+++ src/utils/get-closest-color.js
@@ -4,12 +4,32 @@
 import convert from 'color-convert';
 import tinycolor from 'tinycolor2';
 
-export default function getClosestColor( hex, colorList = {} ) {
+/**
+ * Given a color and a list, get the closest color. If withDiff is provided,
+ * also return the difference value.
+ *
+ * @param {string} hex       The color, usually as a hex code (or rgb string).
+ * @param {Object} colorList A list of colors, with the name as the key, and
+ *                           color value as the value.
+ * @param {boolean} withDiff If the return value should be just the color (false),
+ *                           or an array-pair with the diff value (true).
+ */
+export default function getClosestColor(
+	hex,
+	colorList = {},
+	withDiff = false
+) {
 	if ( ! Object.values( colorList ).length ) {
+		if ( withDiff ) {
+			return [ false, 0 ];
+		}
 		return false;
 	}
 	const color = tinycolor( hex );
 	if ( ! color || ! color.isValid() ) {
+		if ( withDiff ) {
+			return [ false, 0 ];
+		}
 		return false;
 	}
 	const { r, g, b } = color.toRgb(); // @todo handle transparent colors?
@@ -33,5 +53,8 @@ export default function getClosestColor( hex, colorList = {} ) {
 		}
 	} );
 
+	if ( withDiff ) {
+		return [ matchedColor, lowestDiff ];
+	}
 	return matchedColor;
 }
