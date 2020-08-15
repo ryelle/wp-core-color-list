13323843ba05506a2b78218014ef1c007ae96d2f
Kelly Dwan <ryelle@users.noreply.github.com>
2020-08-15 16:04:20 -0400

    Rename brand color files to have same prefix

--- src/colors/index.js
+++ src/colors/index.js
@@ -1,3 +1,10 @@
+/**
+ * List of "brand colors", in name: value pairs.
+ */
 export { default as brandColors } from './brand-colors.json';
-export { default as coreColors } from './core-colors.json';
-export { default as newBrandColors } from './new-brand-colors.json';
+export { default as newBrandColors } from './brand-colors-new.json';
+
+/**
+ * The "messy" list, extracted from WordPress core CSS.
+ */
+export { default as coreColors } from './list-core-css.json';
